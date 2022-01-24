5d613e806ea0082caf16ba018efc7eaebb12275c
Yeison <enmanuelquiterios@gmail.com>
2022-01-24 17:53:58 -0500

    Fixing bug with env constants

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import Products from "./components/Products/Products";
 import NavBar from "./components/NavBar/NavBar";
+import { commerce } from "./lib/commerce";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <NavBar />
