0d3dd6b00d86312afebe9670b5a8a625fcaffb2e
Yeison <enmanuelquiterios@gmail.com>
2022-01-24 17:17:14 -0500

    Added Styles to my NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,15 +8,33 @@ import {
   Menu,
   Typography,
 } from "@material-ui/core";
-import { CallMissedSharp, ShoppingCart } from "@material-ui/icons";
-import { mergeClasses } from "@material-ui/styles";
+import { ShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
+import logo from "../../assets/Logo-two.png";
 
 const NavBar = () => {
+  const classes = useStyles();
   return (
     <div>
       <AppBar postion="fixed" className={classes.appBar} color="inherit">
         <ToolBar>
-          <Typography></Typography>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="ecommerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Ecommerce React App
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
         </ToolBar>
       </AppBar>
     </div>
