b8956007519e451ee1ed06a1f5a3375797ccf507
Yeison <enmanuelquiterios@gmail.com>
2022-01-25 12:07:53 -0500

    Created Items Cart

---
+++ src/components/Cart/CartItem/CartItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+} from "@material-ui/core";
+import useStyles from "./styles";
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardMedia
+        image={item.image.url}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.CardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.CardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button
+          variante="contained"
+          type="button"
+          color="secundary"
+          className={classes.colorCustom}
+        >
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CartItem;
