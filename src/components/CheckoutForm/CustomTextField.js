9ff54256ff4fe64b95cedef4529e8dfb98e49c4f
Yeison <enmanuelquiterios@gmail.com>
2022-01-25 14:28:59 -0500

    Adding TextField Custo Component

--- src/components/CheckoutForm/CustomTextField.js
+++ src/components/CheckoutForm/CustomTextField.js
@@ -2,7 +2,12 @@ import React from "react";
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller } from "react-hook-form";
 const CustomTextField = () => {
-  return <div></div>;
+  const { control } = useFormContext();
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller as={TextField} control={control} />
+    </Grid>
+  );
 };
 
 export default CustomTextField;
