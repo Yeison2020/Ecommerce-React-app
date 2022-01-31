d0defb23bc1dc5c1f67d0ad9f06230d3a9bfcc34
Yeison <enmanuelquiterios@gmail.com>
2022-01-30 22:48:40 -0500

    Edited Readme

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -41,7 +41,7 @@ const Cart = ({
           })}
         </Grid>
         <div className={classes.cardDetails}>
-          <Typography variant="h4">
+          <Typography variant="h4" gutterBottom>
             Subtotal: {cart.subtotal.formatted_with_symbol}
           </Typography>
           <div>
